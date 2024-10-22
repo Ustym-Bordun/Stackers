f6c9a0cd74805972a9b0fd86ed2bda105db508f1
mykhaito <163899158+mykhaito@users.noreply.github.com>
2024-10-22 17:28:15 +0200

    last fixes + link try

--- src/js/close-menu.js
+++ src/js/close-menu.js
@@ -1,13 +1,31 @@
 const closeButton = document.querySelector('.close-button-mobile-menu');
-const mobileMenu = document.querySelector('.container-mobile-menu');
 const burgerMenuButton = document.querySelector('.header-burger-btn');
+const overlayMenu = document.querySelector('.overlay-container-mobile-menu');
+const menuLinks = document.querySelectorAll('.mobile-menu-menu-links a');
+const shopNowButton = document.querySelector('.mobile-menu-shop-now-button');
 
 closeButton.addEventListener('click', () => {
-  mobileMenu.style.visibility = 'hidden';
-  mobileMenu.style.opacity = '0';
+  overlayMenu.classList.remove('is-open');
 });
 
 burgerMenuButton.addEventListener('click', () => {
-  mobileMenu.style.visibility = 'visible';
-  mobileMenu.style.opacity = '1';
+  overlayMenu.classList.add('is-open');
+});
+
+overlayMenu.addEventListener('click', event => {
+  if (event.target === overlayMenu) {
+    overlayMenu.classList.remove('is-open');
+  }
+});
+
+shopNowButton.addEventListener('click', () => {
+  const orderSection = document.querySelector('#order');
+  orderSection.scrollIntoView({ behavior: 'smooth' });
+  overlayMenu.classList.remove('is-open');
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    overlayMenu.classList.remove('is-open');
+  });
 });
