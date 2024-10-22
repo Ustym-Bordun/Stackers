2e1f5f98b627b6364f5df03dfec0eed54dc8b46e
mykhaito <163899158+mykhaito@users.noreply.github.com>
2024-10-22 21:00:21 +0200

    js debug

--- src/js/close-menu.js
+++ src/js/close-menu.js
@@ -1,31 +1,33 @@
-const closeButton = document.querySelector('.close-button-mobile-menu');
-const burgerMenuButton = document.querySelector('.header-burger-btn');
-const overlayMenu = document.querySelector('.overlay-container-mobile-menu');
-const menuLinks = document.querySelectorAll('.mobile-menu-menu-links a');
-const shopNowButton = document.querySelector('.mobile-menu-shop-now-button');
+document.addEventListener('DOMContentLoaded', function () {
+  const closeButton = document.querySelector('.close-button-mobile-menu');
+  const burgerMenuButton = document.querySelector('.header-burger-btn');
+  const overlayMenu = document.querySelector('.overlay-container-mobile-menu');
+  const menuLinks = document.querySelectorAll('.mobile-menu-menu-links a');
+  const shopNowButton = document.querySelector('.mobile-menu-shop-now-button');
 
-closeButton.addEventListener('click', () => {
-  overlayMenu.classList.remove('is-open');
-});
-
-burgerMenuButton.addEventListener('click', () => {
-  overlayMenu.classList.add('is-open');
-});
-
-overlayMenu.addEventListener('click', event => {
-  if (event.target === overlayMenu) {
+  closeButton.addEventListener('click', () => {
     overlayMenu.classList.remove('is-open');
-  }
-});
+  });
 
-shopNowButton.addEventListener('click', () => {
-  const orderSection = document.querySelector('#order');
-  orderSection.scrollIntoView({ behavior: 'smooth' });
-  overlayMenu.classList.remove('is-open');
-});
+  burgerMenuButton.addEventListener('click', () => {
+    overlayMenu.classList.add('is-open');
+  });
 
-menuLinks.forEach(link => {
-  link.addEventListener('click', () => {
+  overlayMenu.addEventListener('click', event => {
+    if (event.target === overlayMenu) {
+      overlayMenu.classList.remove('is-open');
+    }
+  });
+
+  shopNowButton.addEventListener('click', () => {
+    const orderSection = document.querySelector('#order');
+    orderSection.scrollIntoView({ behavior: 'smooth' });
     overlayMenu.classList.remove('is-open');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      overlayMenu.classList.remove('is-open');
+    });
   });
 });
