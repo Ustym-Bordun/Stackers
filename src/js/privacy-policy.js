915907409ce1d103b8605afbd53b953493350001
mykhaito <163899158+mykhaito@users.noreply.github.com>
2024-10-23 17:39:09 +0200

    m

--- src/js/privacy-policy.js
+++ src/js/privacy-policy.js
@@ -1,18 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const refs = {
-    // Додати атрибут data-privacy-open на кнопку відкриття
-    openModalBtn: document.querySelector('[data-privacy-open]'),
-    // Додати атрибут data-privacy-close на кнопку закриття
-    closeModalBtn: document.querySelector('[data-privacy-close]'),
-    // Додати атрибут data-privacy на бекдроп privacy
-    modal: document.querySelector('[data-privacy]'),
+document.addEventListener('DOMContentLoaded', function () {
+  const modalPrivacy = document.querySelector('#privacy');
+  const openModalButton = document.querySelector('.footer-privacy-policy');
+  const closeModalButton = document.querySelector('.btn-privacy-policy');
+
+  const openModal = () => {
+    modalPrivacy.classList.add('is-open');
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  const closeModal = () => {
+    modalPrivacy.classList.remove('is-open');
+  };
 
-  function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-    refs.modal.classList.toggle('is-open');
-  }
+  modalPrivacy.addEventListener('click', event => {
+    if (event.target === modalPrivacy) {
+      modalPrivacy.classList.remove('is-open');
+    }
+  });
+
+  openModalButton.addEventListener('click', event => {
+    event.preventDefault();
+    openModal();
+  });
+
+  closeModalButton.addEventListener('click', closeModal);
 });
